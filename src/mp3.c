3018f72fb2f58308866742a55b892a6d01e14d39
Rainchus <Rainchus@github.com>
2022-02-19 19:01:07 -0600

    finish implementing options

--- src/mp3.c
+++ src/mp3.c
@@ -31,11 +31,14 @@ void playPlayerSadVoice(s32 playerIndex) {
     PlaySound(player->characterID + 0x287); //should play current player sad voice
 }
 
+playerMain* getPlayerStructCustom(s32 playerIndex) {
+	return (playerMain*)0x800D1108 + playerIndex;
+}
+
 void checkPlayerTaunt(s32 playerIndex) { //check if current player in loop should taunt
-	playerMain* player = GetPlayerStruct(playerIndex);
+	playerMain* player = getPlayerStructCustom(playerIndex);
 	if (!(player->flags1 & 1)) { //if player is not cpu
 		s16 currentVoiceOffset = -1;
-		//get ptr to player buttons
 		if ((PlayerPressedButtonsArray[player->controller_port_index] & 0xFFFFFFFF) != 0) { //player has pressed *something*, check what
 			for (s32 j = 0; j < sizeof(tauntButtonsArray) / sizeof(s16); j++) { //check what button was pressed
 				if (PlayerPressedButtonsArray[player->controller_port_index] == tauntButtonsArray[j]) {
@@ -55,13 +58,15 @@ void checkPlayerTaunt(s32 playerIndex) { //check if current player in loop shoul
 
 void playerTauntsMain() {
 	if (globalTaunt == 1) {
-		//global
+		for (s32 i = 0; i < 4; i++) { //iterate over 4 players
+			checkPlayerTaunt(i);
+		}
 	} else {
 		if (inBoardPtr != NULL) { //if in board
 			if (canTauntDuringYourTurn == 1) {
 				for (s32 i = 0; i < 4; i++) { //iterate over 4 players
 					checkPlayerTaunt(i);
-				}	
+				}
 			} else {
 				for (s32 i = 0; i < 4; i++) { //iterate over 4 players
 					if (GetCurrentPlayerIndex() != i) { //if not current player
